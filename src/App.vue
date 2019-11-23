996a5dba5228ae8f84ab37e41b3cdaf50a8cbc10
coderjojo <mail2vedj@gmail.com>
2019-11-24 02:22:36 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="f1">TODO++</h1>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+import image from './assets/logo.jpg'
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+  background-image: url('assets/logo.jpg')
+}
+
+.container {
+   margin: 0 auto;
+  align-items: left;
+
+}
+
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+  text-align: center;
+  text-color: black;
+}
+
+</style>
